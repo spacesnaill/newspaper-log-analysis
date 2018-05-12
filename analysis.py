3309d20ca33f27a4c1da10b603ee692758d32308
Patrick Geiger <patrickjgeiger@gmail.com>
2018-05-12 17:13:30 -0400

    set up cursor for the database

--- analysis.py
+++ analysis.py
@@ -3,12 +3,21 @@ import sys
 
 
 # check if we can connect to the databse
-# if we can connect, good
+# if we can connect, say as much
 # if we cannot connect, then say so and end the program
 try:
-	db = psycopg2.connect("dbname=news")
+	db_connection = psycopg2.connect("dbname=news")
 	print("Successfully connected to the database")
 except:
 	print("Not able to connect to database")
 	sys.exit(1)
+
+# get the db's cursor so we can run queries on the db
+# IMPORTANT NOTE: THIS IS A PSYCOPG CURSOR, NOT A POSTGRESQL CURSOR
+db_cursor = db_connection.cursor()
+
+# using the cursor, queries can be found below
+
+db_cursor.execute(""" """)
+
 
