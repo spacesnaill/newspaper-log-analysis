0c06ff4b6fa89ff38b933d5878b7417583e613eb
Patrick Geiger <patrickjgeiger@gmail.com>
2018-05-12 16:58:51 -0400

    further built on the databse connection try except statement
    Now exits the program if it didn't connect and prints that it connected if it connected

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import psycopg2
+import sys
 
 
 # check if we can connect to the databse
@@ -6,5 +7,8 @@ import psycopg2
 # if we cannot connect, then say so and end the program
 try:
 	db = psycopg2.connect("dbname=news")
+	print("Successfully connected to the database")
 except:
 	print("Not able to connect to database")
+	sys.exit(1)
+
