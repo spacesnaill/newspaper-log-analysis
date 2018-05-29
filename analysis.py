1ee89e63475866513ce5c91e624adf599c4ab21f
Patrick Geiger <patrickjgeiger@gmail.com>
2018-05-29 16:53:03 -0400

    updated to obey PEP8 style guide

--- analysis.py
+++ analysis.py
@@ -24,7 +24,8 @@ file_name = 'query_output.txt'
 output_file = open(file_name, 'w')
 
 # 3 most popular articles
-# returns the title of the article and looks at the log table to see how many times
+# returns the title of the article and 
+# looks at the log table to see how many times
 # it was accessed, counting those times and also displaying that
 try:
 	db_cursor.execute(
@@ -42,24 +43,26 @@ try:
 	output_file.write('\nThree Most Popular Articles:\n')
 
 	for row in rows:
-		output_file.write("Article: {} | Popularity: {} total views\n".format(row[0], row[1]))
-		print("Article: {} | Popularity: {} total views\n".format(row[0], row[1]))
+		output_file.write(
+			"Article: {} | Popularity: {} total views\n".format(row[0], row[1]))
+		print(
+			"Article: {} | Popularity: {} total views\n".format(row[0], row[1]))
 except:
 	output_file.write('Query Failed: Could not get the 3 most popular articles')
 	print('Query Failed: Could not get the 3 most popular articles')
 
-
-
 # Most popular authors
 # returns the author's name and the number of views each of their articles has
-# gets the author name from the author table by matching up the id in articles with the id in authors
+# gets the author name from the author table 
+# by matching up the id in articles with the id in authors
 # counts the number of views each article has
 try:
 	db_cursor.execute(
 		"""
 		SELECT authors.name, COUNT(*) as popularity
 		FROM articles, authors, log
-		WHERE articles.author = authors.id AND '/article/' || articles.slug = path
+		WHERE articles.author = authors.id 
+		AND '/article/' || articles.slug = path
 		GROUP BY authors.name
 		ORDER BY popularity DESC;
 		""")
@@ -69,23 +72,30 @@ try:
 	output_file.write('\nMost Popular Authors:\n')
 
 	for row in rows:
-		output_file.write("Author: {} | Popularity: {} total views\n".format(row[0], row[1]))
-		print("Author: {} | Popularity: {} total views\n".format(row[0], row[1]))
+		output_file.write(
+			"Author: {} | Popularity: {} total views\n".format(row[0], row[1]))
+		print(
+			"Author: {} | Popularity: {} total views\n".format(row[0], row[1]))
 except:
 	output_file.write('Query Failed: Could not get the most popular authors')
 	print('Query Failed: Could not get the most popular authors')
 
-
-
 # On which days more than 1% of requests led to errors
 # Two subqueries form a table with 200 OK messages and 404 messages
-# This could be refactored to just look for the word OK and the word NOT FOUND or other error messages
-# After getting the number of both, we can then perform simple arithmatic to get the error percentage
+# This could be refactored to just look for the word OK 
+# and the word NOT FOUND or other error messages
+# After getting the number of both, 
+# we can then perform simple arithmatic to get the error percentage
 try:
 	db_cursor.execute(
 		"""
-		SELECT TRIM(TO_CHAR(status200.day, 'Month')) || TO_CHAR(status200.day, ', DD YYYY') as long_date,
-		TRUNC(cast(status404.num as decimal) / (status200.num + status404.num), 2) * 100 as error_percentage
+		SELECT TRIM(
+		TO_CHAR(status200.day, 'Month')) || 
+		TO_CHAR(status200.day, ', DD YYYY') as long_date,
+		TRUNC(
+		cast(status404.num as decimal) / 
+		(status200.num + status404.num), 2) * 100 
+		as error_percentage
 		FROM
 		(SELECT status, COUNT(*) as num, date(time) as day
 		FROM log
@@ -101,14 +111,20 @@ try:
 		""")
 	rows = db_cursor.fetchall()
 
-	output_file.write('\nDays in which more than 1% of Requests led to an error:\n')
+	output_file.write(
+		'\nDays in which more than 1% of Requests led to an error:\n')
 	print('\nDays in which more than 1% of Requests led to an error:\n')
 
 	for row in rows:
-		output_file.write("Date: {} | Error Percentage: {}%\n".format(row[0], row[1]))
+		output_file.write(
+			"Date: {} | Error Percentage: {}%\n".format(row[0], row[1]))
 		print("Date: {} | Error Percentage: {}%\n".format(row[0], row[1]))
 except:
-	output_file.write('Query Failed: Could not find on which days more than 1% of requests led to errors')
-	print('Query Failed: Could not find on which days more than 1% of requests led to errors')
+	output_file.write(
+		'Query Failed: ' + 
+		'Could not find on which days more than 1% of requests led to errors')
+	print(
+		'Query Failed: ' + 
+		'Could not find on which days more than 1% of requests led to errors')
 
 output_file.close()
