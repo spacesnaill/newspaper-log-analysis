ffea79c8ad5789620327584a2c89a0adac6ac2c9
Patrick Geiger <patrickjgeiger@gmail.com>
2018-06-06 10:35:02 -0400

    converted tabs to 4 spaces

--- analysis.py
+++ analysis.py
@@ -7,12 +7,12 @@ import sys
 # if we can connect, say as much
 # if we cannot connect, then say so and end the program
 try:
-	db_connection = psycopg2.connect("dbname=news")
-	print("Successfully connected to the database")
+    db_connection = psycopg2.connect("dbname=news")
+    print("Successfully connected to the database")
 except (Exception, psycopg2.DatabaseError) as error:
-	print("Not able to connect to database")
-	print(error)
-	sys.exit(1)
+    print("Not able to connect to database")
+    print(error)
+    sys.exit(1)
 
 # get the db's cursor so we can run queries on the db
 # IMPORTANT NOTE: THIS IS A PSYCOPG CURSOR, NOT A POSTGRESQL CURSOR
@@ -29,31 +29,31 @@ output_file = open(file_name, 'w')
 # looks at the log table to see how many times
 # it was accessed, counting those times and also displaying that
 try:
-	db_cursor.execute(
-		"""
-		SELECT articles.title, views
-		FROM articles
-			INNER JOIN 
-			(SELECT path, count(path) as views
-			FROM log
-			GROUP BY log.path) as log 
-		ON log.path =  '/article/' || articles.slug
-		ORDER BY views DESC
-		LIMIT 3;
-		""")
-	rows = db_cursor.fetchall()
+    db_cursor.execute(
+        """
+        SELECT articles.title, views
+        FROM articles
+            INNER JOIN 
+            (SELECT path, count(path) as views
+            FROM log
+            GROUP BY log.path) as log 
+        ON log.path =  '/article/' || articles.slug
+        ORDER BY views DESC
+        LIMIT 3;
+        """)
+    rows = db_cursor.fetchall()
 
-	print('\nThree Most Popular Articles:\n')
-	output_file.write('\nThree Most Popular Articles:\n')
+    print('\nThree Most Popular Articles:\n')
+    output_file.write('\nThree Most Popular Articles:\n')
 
-	for row in rows:
-		output = "Article: {} | Popularity: {} total views\n".format(row[0], row[1])
-		output_file.write(output)
-		print(output)
+    for row in rows:
+        output = "Article: {} | Popularity: {} total views\n".format(row[0], row[1])
+        output_file.write(output)
+        print(output)
 except (psycopg2.Error) as e:
-	print(e.pgerror)
-	output_file.write('Query Failed: Could not get the 3 most popular articles')
-	print('Query Failed: Could not get the 3 most popular articles')
+    print(e.pgerror)
+    output_file.write('Query Failed: Could not get the 3 most popular articles')
+    print('Query Failed: Could not get the 3 most popular articles')
 
 # Most popular authors
 # returns the author's name and the number of views each of their articles has
@@ -61,28 +61,28 @@ except (psycopg2.Error) as e:
 # by matching up the id in articles with the id in authors
 # counts the number of views each article has
 try:
-	db_cursor.execute(
-		"""
-		SELECT authors.name, COUNT(*) as popularity
-		FROM articles, authors, log
-		WHERE articles.author = authors.id 
-		AND '/article/' || articles.slug = path
-		GROUP BY authors.name
-		ORDER BY popularity DESC;
-		""")
-	rows = db_cursor.fetchall()
+    db_cursor.execute(
+        """
+        SELECT authors.name, COUNT(*) as popularity
+        FROM articles, authors, log
+        WHERE articles.author = authors.id 
+        AND '/article/' || articles.slug = path
+        GROUP BY authors.name
+        ORDER BY popularity DESC;
+        """)
+    rows = db_cursor.fetchall()
 
-	print('\nMost Popular Authors:\n')
-	output_file.write('\nMost Popular Authors:\n')
+    print('\nMost Popular Authors:\n')
+    output_file.write('\nMost Popular Authors:\n')
 
-	for row in rows:
-		output = "Author: {} | Popularity: {} total views\n".format(row[0], row[1])
-		output_file.write(output)
-		print(output)
+    for row in rows:
+        output = "Author: {} | Popularity: {} total views\n".format(row[0], row[1])
+        output_file.write(output)
+        print(output)
 except (psycopg2.Error) as e:
-	print(e.pgerror)
-	output_file.write('Query Failed: Could not get the most popular authors')
-	print('Query Failed: Could not get the most popular authors')
+    print(e.pgerror)
+    output_file.write('Query Failed: Could not get the most popular authors')
+    print('Query Failed: Could not get the most popular authors')
 
 # On which days more than 1% of requests led to errors
 # Two subqueries form a table with 200 OK messages and 404 messages
@@ -91,45 +91,45 @@ except (psycopg2.Error) as e:
 # After getting the number of both, 
 # we can then perform simple arithmatic to get the error percentage
 try:
-	db_cursor.execute(
-		"""
-		SELECT TRIM(
-		TO_CHAR(status200.day, 'Month')) || 
-		TO_CHAR(status200.day, ', DD YYYY') as long_date,
-		TRUNC(
-		cast(status404.num as decimal) / 
-		(status200.num + status404.num), 2) * 100 
-		as error_percentage
-		FROM
-		(SELECT status, COUNT(*) as num, date(time) as day
-		FROM log
-		WHERE status = '200 OK'
-		GROUP BY status, date(time)) status200,
-		(SELECT status, COUNT(*) as num, date(time) as day
-		FROM log
-		WHERE status = '404 NOT FOUND'
-		GROUP BY status, date(time)) status404
-		WHERE status200.day = status404.day AND
-		cast(status404.num as decimal) / (status200.num + status404.num) > 0.01
-		ORDER BY long_date;
-		""")
-	rows = db_cursor.fetchall()
+    db_cursor.execute(
+        """
+        SELECT TRIM(
+        TO_CHAR(status200.day, 'Month')) || 
+        TO_CHAR(status200.day, ', DD YYYY') as long_date,
+        TRUNC(
+        cast(status404.num as decimal) / 
+        (status200.num + status404.num), 2) * 100 
+        as error_percentage
+        FROM
+        (SELECT status, COUNT(*) as num, date(time) as day
+        FROM log
+        WHERE status = '200 OK'
+        GROUP BY status, date(time)) status200,
+        (SELECT status, COUNT(*) as num, date(time) as day
+        FROM log
+        WHERE status = '404 NOT FOUND'
+        GROUP BY status, date(time)) status404
+        WHERE status200.day = status404.day AND
+        cast(status404.num as decimal) / (status200.num + status404.num) > 0.01
+        ORDER BY long_date;
+        """)
+    rows = db_cursor.fetchall()
 
-	output_file.write(
-		'\nDays in which more than 1% of Requests led to an error:\n')
-	print('\nDays in which more than 1% of Requests led to an error:\n')
+    output_file.write(
+        '\nDays in which more than 1% of Requests led to an error:\n')
+    print('\nDays in which more than 1% of Requests led to an error:\n')
 
-	for row in rows:
-		output = "Date: {} | Error Percentage: {}%\n".format(row[0], row[1])
-		output_file.write(output)
-		print(output)
+    for row in rows:
+        output = "Date: {} | Error Percentage: {}%\n".format(row[0], row[1])
+        output_file.write(output)
+        print(output)
 except (psycopg2.Error) as e:
-	print(e.pgerror)
-	output_file.write(
-		'Query Failed: ' + 
-		'Could not find on which days more than 1% of requests led to errors')
-	print(
-		'Query Failed: ' + 
-		'Could not find on which days more than 1% of requests led to errors')
+    print(e.pgerror)
+    output_file.write(
+        'Query Failed: ' + 
+        'Could not find on which days more than 1% of requests led to errors')
+    print(
+        'Query Failed: ' + 
+        'Could not find on which days more than 1% of requests led to errors')
 
 output_file.close()
